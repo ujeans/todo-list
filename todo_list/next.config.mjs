9dcf02f9124f050f0a5a2f09bf1ce8b504eed1a3
ujeans <dbwlsxkal95@gmail.com>
2024-09-02 12:41:31 +0900

    next.config.mjs 파일에 @svgr/webpack 설정 추가하기

--- todo_list/next.config.mjs
+++ todo_list/next.config.mjs
@@ -1,4 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // 클라이언트 빌드에서 fs 모듈 무시
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        module: false,
+        path: false,
+        os: false,
+      };
+    }
+
+    // SVG를 React 컴포넌트로 변환하는 설정 추가
+    config.module.rules.push({
+      test: /\.svg$/, // .svg 파일을 대상으로 함
+      use: ["@svgr/webpack"], // @svgr/webpack 로더 사용
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
